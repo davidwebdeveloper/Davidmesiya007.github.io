29b8daa750459d4fc16157087cf965f28ea22b9e
David M <mddavid11204@gmail.com>
2023-02-14 22:46:40 +0530

    Domain name changedd

---
+++ js/email.js
@@ -0,0 +1,22 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_u6a4ugp";
+  const templateID = "template_1z20tzb";
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("subject").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("your message was send successfully");
+    })
+    .catch((err) => console.log(err));
+}
